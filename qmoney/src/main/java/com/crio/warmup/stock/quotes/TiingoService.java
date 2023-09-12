6b236fd3be16a6184563acc2494276d0c590a31d
CrioUser <crio-user@criodo.com>
2023-09-12 13:04:11 +0530

    qmoney_commit

--- qmoney/src/main/java/com/crio/warmup/stock/quotes/TiingoService.java
+++ qmoney/src/main/java/com/crio/warmup/stock/quotes/TiingoService.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import org.springframework.web.client.RestTemplate;
 
+
 public class TiingoService implements StockQuotesService {
 
   public static final String token = "d7ee5290251fd4882f10fde8ada179ccc1450745";
